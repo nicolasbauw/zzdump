1057234d2245ba40a6f453c7b455f63e4994aaef
Nicolas BAUW <nbauw@hotmail.com>
2025-02-10 14:22:19 +0100

    Timestamp as argument

--- src/main.zig
+++ src/main.zig
@@ -12,7 +12,7 @@ pub fn main() !void {
     defer timezone.close();
 
     // Getting last timechange data
-    const last = getLastTT(timezone.time_data);
+    const last = getPrevTT(timezone.time_data, std.time.timestamp());
     const last_ttinfo = timezone.time_types[timezone.time_indices[last[1]]];
 
     // Getting abbreviation for this timetype
@@ -27,31 +27,30 @@ pub fn main() !void {
         }
     }
 
-    std.debug.print("Current timestamp          : {any}\n", .{std.time.timestamp()});
-    std.debug.print("Latest change timestamp    : {any}\n", .{last[0]});
-    std.debug.print("Latest change ttinfo       : {any}\n", .{last_ttinfo});
-    std.debug.print("Latest change abbr         : {s}\n", .{timezone.tt_desig[start_index..end_index]});
+    std.debug.print("Current timestamp      : {any}\n", .{std.time.timestamp()});
+    std.debug.print("Last change timestamp  : {any}\n", .{last[0]});
+    std.debug.print("Last change ttinfo     : {any}\n", .{last_ttinfo});
+    std.debug.print("Last change abbr       : {s}\n", .{timezone.tt_desig[start_index..end_index]});
 
     std.process.cleanExit();
 }
 
-fn getLastTT(timecnt: []const i64) struct { i64, usize } {
-    const current = std.time.timestamp();
-    var latest_tt: struct { i64, usize } = .{ undefined, undefined };
+fn getPrevTT(timecnt: []const i64, ref_time: i64) struct { i64, usize } {
+    var last_tt: struct { i64, usize } = .{ undefined, undefined };
 
     for (timecnt, 0..) |time, index| {
-        if (time < current) {
-            latest_tt[0] = time;
-            latest_tt[1] = index;
+        if (time < ref_time) {
+            last_tt[0] = time;
+            last_tt[1] = index;
         }
     }
 
-    return latest_tt;
+    return last_tt;
 }
 
 test "get last timechange" {
     const timezone = try tzfile.Tz.open(std.testing.allocator, "/usr/share/zoneinfo/America/Phoenix");
     defer timezone.close();
-    const last = getLastTT(timezone.time_data);
+    const last = getPrevTT(timezone.time_data);
     try std.testing.expectEqual(last[0], @as(i64, -68659200));
 }
