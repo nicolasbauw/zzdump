342cd048bf19168c61ef8378dde6658f6c7ec9cd
Nicolas BAUW <nbauw@hotmail.com>
2025-02-15 21:14:50 +0100

    Tzfile TT * 1000 -> DateTime

--- src/main.zig
+++ src/main.zig
@@ -39,6 +39,7 @@ pub fn main() void {
     std.debug.print("Current timestamp      : {any}\n", .{ts});
     std.debug.print("Current date/time      : {any}\n", .{dt});
     std.debug.print("Last change timestamp  : {any}\n", .{last[0]});
+    std.debug.print("Last change date/time  : {any}\n", .{DateTime.fromMillis(last[0] * 1000)});
     std.debug.print("Last change ttinfo     : {any}\n", .{last_ttinfo});
     std.debug.print("Last change abbr       : {s}\n", .{timezone.tt_desig[start_index..end_index]});
 
